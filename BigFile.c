50a90e160651252d52db5e84805d1e17cc028db6
SeenLex <draghicidenis@yahoo.com>
2023-05-06 15:23:07 +0300

    First Part - DONE

--- BigFile.c
+++ BigFile.c
@@ -1,15 +1,149 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <ctype.h>
+#include <errno.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <sys/types.h>
-#include <errno.h>
-#include <sys/time.h>
-#include <time.h>
+#include <stdlib.h>
+#include <dirent.h>
 
 #define MAX_OPTIONS 10
+
+void directory_options_selector(char *option, char *filename) {
+    if (strcmp(option, "-n") == 0) {
+        printf("Directory name: %s\n", filename);
+    } else if (strcmp(option, "-d") == 0) {
+        struct stat sb;
+        if (stat(filename, &sb) == -1) {
+            perror("stat");
+            return;
+        }
+        printf("Directory size: %lld bytes\n", (long long)sb.st_size);
+    } else if (strcmp(option, "-a") == 0) {
+        print_access_rights(filename);
+    } else if (strcmp(option, "-c") == 0) {
+        int count = 0;
+        DIR *dir;
+        struct dirent *entry;
+
+        dir = opendir(filename);
+        if (dir == NULL) {
+            perror("opendir");
+            return;
+        }
+
+        while ((entry = readdir(dir)) != NULL) {
+            struct stat sb;
+            char path[256];
+            snprintf(path, sizeof(path), "%s/%s", filename, entry->d_name);
+            if (stat(path, &sb) == -1) {
+                perror("stat");
+                continue;
+            }
+            if (S_ISREG(sb.st_mode)) {
+                const char *ext = strrchr(entry->d_name, '.');
+                if (ext != NULL && strcmp(ext, ".c") == 0) {
+                    count++;
+                }
+            }
+        }
+
+        closedir(dir);
+        printf("Total number of files with .c extension: %d\n", count);
+    } else {
+        printf("Invalid option: %s\n", option);
+    }
+}
+
+
+// void symlink_options_selector(char *option, char *filename) {
+//     if (strcmp(option, "-n") == 0) {
+//         printf("File name: %s\n", filename);
+//     } else if (strcmp(option, "-d") == 0) {
+//         struct stat sb;
+//         if (lstat(filename, &sb) == -1) {
+//             perror("lstat");
+//             return;
+//         }
+//         printf("Size of symbolic link: %lld bytes\n", (long long)sb.st_size);
+//     } else if (strcmp(option, "-l") == 0) {
+//         if (unlink(filename) == -1) {
+//             perror("unlink");
+//         } else {
+//             printf("Symbolic link deleted successfully.\n");
+//         }
+//     } else if (strcmp(option, "-t") == 0) {
+//         struct stat sb;
+//         if (lstat(filename, &sb) == -1) {
+//             perror("lstat");
+//             return;
+//         }
+//         if (S_ISLNK(sb.st_mode)) {
+//             char target[256];
+//             ssize_t targetSize = readlink(filename, target, sizeof(target) - 1);
+//             if (targetSize == -1) {
+//                 perror("readlink");
+//             } else {
+//                 target[targetSize] = '\0';
+//                 struct stat target_sb;
+//                 if (stat(target, &target_sb) == -1) {
+//                     perror("stat");
+//                 } else {
+//                     printf("Size of target file: %lld bytes\n", (long long)target_sb.st_size);
+//                 }
+//             }
+//         } else {
+//             printf("Not a symbolic link.\n");
+//         }
+//     } else if (strcmp(option, "-a") == 0) {
+//         print_access_rights(filename);
+//     } else {
+//         printf("Invalid option: %s\n", option);
+//     }
+// }
+
+
+void regular_file_options_selector(char *option, char *filename) {
+    if (strcmp(option, "-n") == 0) {
+        printf("File name: %s\n", filename);
+    } else if (strcmp(option, "-d") == 0) {
+        struct stat sb;
+        if (stat(filename, &sb) == -1) {
+            perror("stat");
+            return;
+        }
+        printf("File size: %lld bytes\n", (long long)sb.st_size);
+    } else if (strcmp(option, "-h") == 0) {
+        struct stat sb;
+        if (stat(filename, &sb) == -1) {
+            perror("stat");
+            return;
+        }
+        printf("Hard link count: %ld\n", (long)sb.st_nlink);
+    } else if (strcmp(option, "-m") == 0) {
+        struct stat sb;
+        if (stat(filename, &sb) == -1) {
+            perror("stat");
+            return;
+        }
+        printf("Time of last modification: %s", ctime(&sb.st_mtime));
+    } else if (strcmp(option, "-a") == 0) {
+        print_access_rights(filename);
+    } else if (strcmp(option, "-l") == 0) {
+        // Create symbolic link (requires input)
+    //     char target[256];
+    //     printf("Enter the target path for the symbolic link: ");
+    //     scanf("%255s", target);
+    //     if (symlink(target, filename) == -1) {
+    //         perror("symlink");
+    //     } else {
+    //         printf("Symbolic link created successfully.\n");
+    //     }
+    } else {
+        printf("Invalid option: %s\n", option);
+    }
+}
 
 void print_access_rights(const char *path) {
     struct stat sb;
@@ -38,7 +172,7 @@ void print_access_rights(const char *path) {
 void print_file_info(int noOfFiles, const char *path[]) {
     
     struct stat sb;
-
+    char options[2];
 
     for(int i = 1; i <= noOfFiles; i++){
         if (stat(path[i], &sb) == -1) {
@@ -57,6 +191,10 @@ void print_file_info(int noOfFiles, const char *path[]) {
                 printf("-m: Time of last modification\n");
                 printf("-a: Access rights\n");
                 printf("-l: Create symbolic link (requires input)\n");
+                
+                printf("Enter option: ");
+                scanf("%s", options);
+                regular_file_options_selector(options, path[i]);
                 break;
             // case S_IFLNK:
             //     printf("Symbolic link\n");
@@ -66,6 +204,10 @@ void print_file_info(int noOfFiles, const char *path[]) {
             //     printf("-d: Size of symbolic link\n");
             //     printf("-t: Size of target file\n");
             //     printf("-a: Access rights\n");
+
+                // printf("Enter option: ");
+                // scanf("%s", options);
+                // symlink_options_selector(options, path[i]);
             //     break;
             case S_IFDIR:
                 printf("Directory\n");
@@ -74,6 +216,10 @@ void print_file_info(int noOfFiles, const char *path[]) {
                 printf("-d: Size\n");
                 printf("-a: Access rights\n");
                 printf("-c: Total number of files with .c extension\n");
+
+                printf("Enter option: ");
+                scanf("%s", options);
+                directory_options_selector(options, path[i]);
                 break;
             default:
                 printf("Unknown\n");
@@ -171,9 +317,9 @@ void print_file_info(int noOfFiles, const char *path[]) {
 // }
 
 
-int main(int argc, char const *argv[]){
+int main(int argc, char *argv[]){
     
-    
+    //compile_c_file(argv[1]);
     print_file_info(argc, argv);
     return 0;
 }
